14a0859b8f2460dae2d525fb6b5273ba66d2181b
MohammedSalama <mohamed96salama@gmail.com>
2023-08-21 02:09:18 +0200

    implement frontend theme in ecommerce project

---
+++ routes/dashboard.php
@@ -0,0 +1,15 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register dashboard routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "dashboard" middleware group. Make something great!
+|
+*/
+
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Livewire\HomeComponent;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,9 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/',HomeComponent::class)->name('home.index');
 
 Route::middleware([
     'auth:sanctum',
