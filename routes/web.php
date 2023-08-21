09122a819cd782e3732c3206e8d281a542ee543f
MohammedSalama <mohamed96salama@gmail.com>
2023-08-21 07:33:26 +0200

    changing the login and register page layout

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/', function () {
-//    return view('welcome');
-//});
+Route::get('/', function () {
+    return view('auth.login');
+        redirect()->route('home.index');
+});
 
 Route::middleware([
     'auth:sanctum',
