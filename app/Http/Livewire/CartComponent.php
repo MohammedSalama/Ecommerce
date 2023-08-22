2639b946fbb17583f87394db446c43e450af1c73
MohammedSalama <mohamed96salama@gmail.com>
2023-08-22 07:57:36 +0200

    update cart quantity and delete cart item

--- app/Http/Livewire/CartComponent.php
+++ app/Http/Livewire/CartComponent.php
@@ -3,9 +3,47 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Gloudemans\Shoppingcart\Facades\Cart;
 
 class CartComponent extends Component
 {
+    /**
+     * @param $rowId
+     * @return void
+     */
+    public function increaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty + 1;
+        Cart::update($rowId,$qty);
+    }
+
+    /**
+     * @param $rowId
+     * @return void
+     */
+    public function decreaseQuantity($rowId)
+    {
+        $product = Cart::get($rowId);
+        $qty = $product->qty - 1;
+        Cart::update($rowId,$qty);
+    }
+
+    /**
+     * @param $id
+     * @return void
+     */
+    public function destroy($id)
+    {
+        Cart::remove($id);
+        session()->flash('success_message','Item has been removed');
+    }
+
+    public function clearAll()
+    {
+        Cart::destroy();
+    }
+
     /**
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View|\Illuminate\Foundation\Application
      */
