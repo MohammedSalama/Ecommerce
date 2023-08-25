a591f4d5ecc095bff6e0466ea56cfaf14dd330a1
MohammedSalama <mohamed96salama@gmail.com>
2023-08-25 19:22:14 +0200

    implement product page in admin ecommerce

---
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -0,0 +1,80 @@
+<div>
+    <style>
+        nav svg {
+            height: 20px;
+        }
+
+        nav .hidden {
+            display: block;
+        }
+    </style>
+    <main class="main">
+        <div class="page-header breadcrumb-wrap">
+            <div class="container">
+                <div class="breadcrumb">
+                    <a href="{{ route('home.index') }}" rel="nofollow">Home</a>
+                    <span></span> All Products
+                </div>
+            </div>
+        </div>
+        <section class="mt-50 mb-50">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="card">
+                            <div class="card-header">
+                                <div class="row">
+                                    <div class="col-md-6">
+                                        All Products
+                                    </div>
+                                    <div class="col-md-6">
+
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="card-body">
+                                @if(Session::has('message'))
+                                    <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                                @endif
+                                <table class="table table-striped">
+                                    <thead>
+                                    <tr>
+                                        <td>#</td>
+                                        <td>Image</td>
+                                        <td>Name</td>
+                                        <td>Stock</td>
+                                        <td>Price</td>
+                                        <td>Category</td>
+                                        <td>Date</td>
+                                        <td>Action</td>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    @php
+                                        $i = ($products->currentPage()-1)*$products->perPage();
+                                    @endphp
+                                    @foreach($products as $product)
+                                        <tr>
+                                            <td>{{ ++$i }}</td>
+                                            <td><img src="{{ asset('layouts/assets/imgs/shop/product-')}}{{ $product->id }}-1.jpg" alt="{{ $product->name }}" width="60" /></td>
+                                            <td>{{ $product->name }}</td>
+                                            <td>{{ $product->stock_status }}</td>
+                                            <td>{{ $product->regular_price }}</td>
+                                            <td>{{ $product->category->name }}</td>
+                                            <td>{{ $product->created_at }}</td>
+                                            <td>
+
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                                {{ $products->links() }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+</div>
