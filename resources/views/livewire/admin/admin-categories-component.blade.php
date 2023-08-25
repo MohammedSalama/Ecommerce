e3926683f19b83a535a8a869997e3d77be3920c2
MohammedSalama <mohamed96salama@gmail.com>
2023-08-25 18:17:58 +0200

    implement delete category from admin ecommerce

--- resources/views/livewire/admin/admin-categories-component.blade.php
+++ resources/views/livewire/admin/admin-categories-component.blade.php
@@ -34,6 +34,9 @@
                                 </div>
                             </div>
                             <div class="card-body">
+                                @if(Session::has('message'))
+                                    <div class="alert alert-success" role="alert">{{ Session::get('message') }}</div>
+                                @endif
                                 <table class="table table-striped">
                                     <thead>
                                     <tr>
@@ -54,7 +57,7 @@
                                             <td>{{ $category->slug }}</td>
                                             <td>
                                                 <a href="{{ route('admin.category.edit',['category_id' => $category->id]) }}" class="text-info">Edit</a>
-                                                <a href=""></a>
+                                                <a href="#" class="text-danger" onclick="deleteConfirmation({{$category->id}})" style="margin-left: 20px;">Delete</a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -69,3 +72,35 @@
         </section>
     </main>
 </div>
+
+<div class="modal" id="deleteConfirmation">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-body pb-30 pt-30">
+                <div class="row">
+                    <div class="col-md-12 text-center">
+                        <h4 class="pb-3">Do you want to delete this record !</h4>
+                        <button type="button" class="btn btn-secondary" data-bs-toggle="modal" data-bs-target="#deleteConfirmation">Cancel</button>
+                        <button type="button" class="btn btn-danger" onclick="deleteCategory()">Delete</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@push('scripts')
+    <script>
+        function deleteConfirmation(id)
+        {
+            @this.set('category_id',id);
+            $('#deleteConfirmation').modal('show');
+        }
+
+        function deleteCategory()
+        {
+            @this.call('deleteCategory');
+            $('#deleteConfirmation').modal('hide');
+        }
+    </script>
+@endpush
